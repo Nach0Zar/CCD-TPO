4d04f4ea580562c8e7723beb568d3c6cc3d8da2e
Rafael Gini - DevRev <rafagini6@gmail.com>
2025-11-24 13:48:12 -0300

    Update HistoricConsumption.tsx

--- frontend/src/pages/HistoricConsumption.tsx
+++ frontend/src/pages/HistoricConsumption.tsx
@@ -1,5 +1,6 @@
 import BrazilMapLayers from "@/components/BrazilMapLayers";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import GeoEconomicPanel from "@/modules/historical/GeoEconomicPanel";
 import HistoricalConsumptionPanel from "@/modules/historical/HistoricalConsumptionPanel";
 import TimeSeriesChart from "@/modules/historical/TimeSeriesChart";
 import ReactECharts from "echarts-for-react";
@@ -181,6 +182,7 @@ const HistoricConsumption = () => {
         <div className="grid gap-6 mb-6">
 
           <HistoricalConsumptionPanel />
+          <GeoEconomicPanel />
           <BrazilMapLayers />
           <Card className="shadow-lg">
             <CardHeader>
